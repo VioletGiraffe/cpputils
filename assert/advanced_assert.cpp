7c9ba24e2909376baed79299a450d7ad6e54b3c5
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2015-07-09 20:10:01 +0300

    Debug / release macros check added (required for normal assert functioning on non-Windows systems)

--- assert/advanced_assert.cpp
+++ assert/advanced_assert.cpp
@@ -1,5 +1,9 @@
 #include "advanced_assert.h"
 
+#if defined _DEBUG == (defined NDEBUG || NDEBUG == 1)
+#error "Either _DEBUG or NDEBUG=1 must be defined"
+#endif
+
 std::function<void (const char*)> AdvancedAssert::_loggingFunc;
 
 void AdvancedAssert::setLoggingFunc(const std::function<void (const char*)>& func)
