51e370cc507ff8258b5e2812e4c8e022073269ac
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-07-10 12:43:34 +0300

    FNV_1a_64 code style issue fixed

--- hash/fnv_1a.h
+++ hash/fnv_1a.h
@@ -5,11 +5,8 @@
 inline uint64_t FNV_1a_64(const void* const data, const size_t length)
 {
 	uint64_t hash = 14695981039346656037ULL;
-	for (const uint8_t* bytePtr = (const uint8_t*)data, *end = bytePtr + length; bytePtr != end; ++bytePtr)
-	{
-		hash ^= *bytePtr;
-		hash *= 1099511628211ULL;
-	}
+	for (const char* bytePtr = static_cast<const char*>(data), *end = bytePtr + length; bytePtr != end; ++bytePtr)
+		hash = (hash ^ static_cast<uint64_t>(*bytePtr)) * 1099511628211ULL;
 
 	return hash;
 }
