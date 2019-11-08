5c1fdf5a545faaea297f9167f263e50c1a12f5cf
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-11-09 00:14:20 +0200

    advanced_assert improved

--- assert/advanced_assert.h
+++ assert/advanced_assert.h
@@ -47,13 +47,17 @@ private:
 #if defined _DEBUG || !defined NDEBUG
 #define assert_debug_only(condition) DISABLE_COMPILER_WARNINGS assert_without_abort(condition); RESTORE_COMPILER_WARNINGS
 #else
-#define assert_debug_only(condition)
+#define assert_debug_only(condition) (void)0 // Without this, 'if (x) assert_debug_only(y); else z;' would not be valid
 #endif
 
 // This macro checks the condition and prints failed assertions to the log both in debug and release. Actual assert is only triggered in debug.
-#define assert_r(condition) {if (!(condition)) {AdvancedAssert::logAssertion(#condition, __FUNCTION__, __LINE__); assert_debug_only(condition);}}
-#define assert_message_r(condition, message) {if (!(condition)) {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__); assert_debug_only(condition);}}
-#define assert_unconditional_r(message) {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__); assert_debug_only(false);}
-#define assert_and_return_r(condition, returnValue) {if (!(condition)) {AdvancedAssert::logAssertion(#condition, __FUNCTION__, __LINE__); assert_debug_only(condition); return returnValue;}}
-#define assert_and_return_unconditional_r(message, returnValue) {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__); assert_debug_only(false); return returnValue;}
-#define assert_and_return_message_r(condition, message, returnValue) {if (!(condition)) {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__); assert_debug_only(condition); return returnValue;}}
+
+// This implementation as an expression plays nicely with constructs like 'if (x) hlassert(y); else z;'
+// and lets you terminate hlassert with ';' in the calling code without breaking logic
+#define assert_r(condition) do {if (!static_cast<bool>(condition)) {AdvancedAssert::logAssertion(#condition, __FUNCTION__, __LINE__); assert_debug_only(condition);}} while(0)
+#define assert_message_r(condition, message) do {if (!static_cast<bool>(condition)) {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__); assert_debug_only(condition);}} while(0)
+#define assert_unconditional_r(message) do {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__), assert_debug_only(! #message);} while(0)
+
+#define assert_and_return_r(condition, returnValue) do {if (!static_cast<bool>(condition)) {AdvancedAssert::logAssertion(#condition, __FUNCTION__, __LINE__); assert_debug_only(condition); return returnValue;}} while(0)
+#define assert_and_return_unconditional_r(message, returnValue) do {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__); assert_debug_only(! #message); return returnValue;} while(0)
+#define assert_and_return_message_r(condition, message, returnValue) do {if (!static_cast<bool>(condition)) {AdvancedAssert::logMessage(message, __FUNCTION__, __LINE__); assert_debug_only(condition); return returnValue;}} while(0)
