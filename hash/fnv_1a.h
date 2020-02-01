ef02ac89cfdef3b48645269a14220d5c5ea67958
VioletGiraffe <violet.giraffe@noreply.github.com>
2020-02-01 23:01:20 +0200

    FNV_1a_64 stateful hasher added

--- hash/fnv_1a.h
+++ hash/fnv_1a.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <type_traits>
 
 inline uint64_t FNV_1a_64(const void* const data, const size_t length)
 {
@@ -10,3 +11,21 @@ inline uint64_t FNV_1a_64(const void* const data, const size_t length)
 
 	return hash;
 }
+
+class FNV_1a_64_hasher {
+public:
+	template <typename T>
+	void updateHash(const T& value) noexcept {
+		static_assert(std::is_trivial_v<T>);
+		for (const char* bytePtr = reinterpret_cast<const char*>(std::addressof(value)), *end = bytePtr + sizeof(value); bytePtr != end; ++bytePtr)
+			_hash = (_hash ^ static_cast<uint64_t>(*bytePtr)) * 1099511628211ULL;
+	}
+
+	inline uint64_t hash() const noexcept {
+		return _hash;
+	}
+
+private:
+	uint64_t _hash = 14695981039346656037ULL;
+};
+
